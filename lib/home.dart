a4f41fe7bd5a22f610fde42aed3798cc37b0b411
uumair327 <mohdumair.a@somaiya.edu>
2024-08-13 06:36:06 +0530

    Floating button

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ims/item.dart';
+import 'package:ims/item_data.dart';
 import 'package:ims/item_list.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -12,6 +15,40 @@ class Home extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Add functionality to add a new item
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              String newItem = '';
+              return AlertDialog(
+                title: Text('Add New Item'),
+                content: TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(hintText: "Enter item name"),
+                  onChanged: (value) {
+                    newItem = value;
+                  },
+                ),
+                actions: [
+                  TextButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Add'),
+                    onPressed: () {
+                      if (newItem.isNotEmpty) {
+                        Provider.of<ItemData>(context, listen: false)
+                            .addItem(Item(item: newItem));
+                      }
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
         },
         child: Icon(Icons.add),
       ),
